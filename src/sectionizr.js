7e966d8ef4eec8d40c6d306a801ae937c4c5c3b5
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2017-07-20 10:34:41 +0100

    Optimized to keep out-of-view sections, truly out of view, so they don't interfere while tabbing

--- src/sectionizr.js
+++ src/sectionizr.js
@@ -25,31 +25,31 @@ $.fn.sectionize = $.fn.sectionizr = function () {
 
             // behaviours
             snzr.next = () => {
-                if (!snzr.hasNext()) 
+                if (!snzr.hasNext())
                     return false
                 return offset()
             }
             snzr.prev = () => {
-                if (!snzr.hasPrev()) 
+                if (!snzr.hasPrev())
                     return false
                 return offset(false)
             }
             snzr.step = step => {
                 snzr.position += step
-                snzr.position = snzr.position < 1
-                    ? 1
-                    : (snzr.position > snzr.sections.length
-                        ? snzr.sections.length
-                        : snzr.position)
+                snzr.position = snzr.position < 1 ?
+                    1 :
+                    (snzr.position > snzr.sections.length ?
+                        snzr.sections.length :
+                        snzr.position)
                 offset(null)
             }
             snzr.go = where => {
                 snzr.position = where
-                snzr.position = snzr.position < 1
-                    ? 1
-                    : (snzr.position > snzr.sections.length
-                        ? snzr.sections.length
-                        : snzr.position)
+                snzr.position = snzr.position < 1 ?
+                    1 :
+                    (snzr.position > snzr.sections.length ?
+                        snzr.sections.length :
+                        snzr.position)
                 offset(null)
             }
             snzr.first = () => {
@@ -66,21 +66,38 @@ $.fn.sectionize = $.fn.sectionizr = function () {
 
             // local
             function offset(forward = true) {
-                (forward === true
-                    ? snzr.position ++: (forward === false
-                        ? snzr.position--
-                        : null))
-                let _prop = isVertical
-                    ? 'marginTop'
-                    : 'marginLeft'
+                (forward === true ?
+                    snzr.position++ : (forward === false ?
+                        snzr.position--
+                        :
+                        null))
+                let _prop = isVertical ?
+                    'marginTop' :
+                    'marginLeft'
                 let _offset = `-${ (snzr.position - 1) * (isVertical
                     ? snzr.sections[0].clientHeight
                     : 100)}${isVertical
                     ? 'px'
                     : '%'}`
-                $(snzr.sections[0]).css({[_prop]: _offset})
+                $(snzr.sections[0]).css({
+                    [_prop]: _offset
+                })
+
+                // manage visibility : collapsing out-of-view sections keeps them from being tabbed into
+                $('.snzr').removeClass('snzr')
+                $('.snzr-visible').removeClass('snzr-visible')
+                _delay(() => {
+                    $(snzr.sections[snzr.position - 1]).addClass('snzr-visible')
+                    $(snzr.sections).addClass('snzr')
+                }, 500)
+
                 return true
             }
+
+            function _delay(cb, d) {
+                setTimeout(cb, d)
+            }
+
             function init() {
                 isVertical = $(snzr.el).hasClass('vertical')
                 snzr.sections.length = 0
@@ -92,9 +109,9 @@ $.fn.sectionize = $.fn.sectionizr = function () {
         })()
         guys.push(el.sectionizr)
     })
-    return guys.length == 1
-        ? guys[0]
-        : guys
+    return guys.length == 1 ?
+        guys[0] :
+        guys
 }
-if (typeof module != 'undefined' && module.exports) 
+if (typeof module != 'undefined' && module.exports)
     module.exports = {}
